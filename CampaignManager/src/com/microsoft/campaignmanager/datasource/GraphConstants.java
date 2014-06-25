7c347f556848cfed1927e8fce8c06071847197f1
Thomas Mechelke <tmech@tmechlabs.com>
2014-06-25 15:49:39 +0200

    Ran source code cleanup in eclipse

--- CampaignManager/src/com/microsoft/campaignmanager/datasource/GraphConstants.java
+++ CampaignManager/src/com/microsoft/campaignmanager/datasource/GraphConstants.java
@@ -3,31 +3,31 @@ package com.microsoft.campaignmanager.datasource;
 import java.nio.charset.Charset;
 
 public class GraphConstants {
-	
+
 	/**
 	 * HTTP GET Verb
 	 */
 	public static final String HTTP_GET = "GET";
-	
+
 	/**
 	 * HTTP POST Verb
 	 */
 	public static final String HTTP_POST = "POST";
-	
+
 	/**
 	 * UTF-8 Encoding name
 	 */
 	public static final String UTF8_NAME = "UTF-8";
-	
+
 	/**
 	 * UTF-8 Charset instance
 	 */
 	public static final Charset UTF8 = Charset.forName(UTF8_NAME);
-	
+
 	public static final String AAD_LOGINURL = "https://login.windows.net";
-	
+
 	public static final String AAD_GRAPHURL = "https://graph.windows.net";
-	
+
 	// AAD_SetupAppId and AAD_SetupRedirectUrl are configured
 	// in Azure AD owned by Microsoft.
 	// On first launch, the app will display a configuration screen
@@ -38,8 +38,8 @@ public class GraphConstants {
 	// If you are uncomfortable giving a Microsoft app management permissions
 	// on your tenant's Azure AD, you can manually configure an Application
 	// as described in the setup instructions
-	
-	//TODO set this to a client ID in a Microsoft Azure tenant
+
+	// TODO set this to a client ID in a Microsoft Azure tenant
 	public static final String AAD_SetupAppId = "0ac9da4c-4fef-4738-aab6-5745596ff44f";
 	public static final String AAD_SetupRedirectUrl = "http://localhost";
 
